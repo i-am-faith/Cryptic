d63d8e671e277055979aafd4d3d84dde88f4d84a
Sourin <sourin.mukherjee2580@gmail.com>
2024-07-21 04:08:21 +0530

    keras

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
+import keras
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import streamlit as st
